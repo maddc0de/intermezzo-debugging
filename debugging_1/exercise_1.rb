9e8cef4d7d02bb1556dfc1a188084aa068d643e7
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-16 13:37:17 +0000

    project setup

---
+++ debugging_1/exercise_1.rb
@@ -0,0 +1,16 @@
+# Supplied code:
+#
+# def say_hello(name)
+#   return "hello #(name)"
+# end
+
+# Intended output:
+#
+# > say_hello("kay")
+# => "hello kay"
+
+# Debugged code:
+#
+def say_hello(name)
+  return "hello #{name}"
+end
