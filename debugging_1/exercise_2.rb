bf960640a9acaba900b36fb6b609de7d256ad536
Madelane <madelanedaz@gmail.com>
2023-03-16 14:00:52 +0000

    debug exercise 2 for intermezzo debugging 1

--- debugging_1/exercise_2.rb
+++ debugging_1/exercise_2.rb
@@ -25,10 +25,9 @@
 # => "theswiftfoxjumpedoverthelazydog"
 
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    puts cipher.find_index(char)
-    (65 + cipher.find_index(char)).chr ## problem line ##
+    (65 + cipher.find_index(char)).chr
   end
   return ciphertext_chars.join
 end
